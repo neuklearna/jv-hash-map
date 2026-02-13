5436a6e4041c655ed12961b2d00bf8f9d9bff46d
Martyna Szałańska <szalanskatita2002@gmail.com>
2026-02-13 16:10:17 +0100

    Fix whitespace and line separators in Node class

--- src/main/java/core/basesyntax/Node.java
+++ src/main/java/core/basesyntax/Node.java
@@ -1,15 +1,16 @@
 package core.basesyntax;
 
-public class Node<K,V> {
+class Node<K, V> {
     private final K key;
     private V value;
-    private Node <K,V> next;
+    private Node<K, V> next;
 
-    public Node(K key, V value, Node <K,V> next) {
+    Node(K key, V value, Node<K, V> next) {
         this.key = key;
-        this.next = next;
         this.value = value;
+        this.next = next;
     }
+
     public K getKey() {
         return key;
     }
@@ -18,15 +19,15 @@ public class Node<K,V> {
         return value;
     }
 
-    public Node <K,V> getNext() {
-        return next;
-    }
-
     public void setValue(V value) {
         this.value = value;
     }
 
-    public void setNext(Node <K,V> next) {
+    public Node<K, V> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<K, V> next) {
         this.next = next;
     }
 }
